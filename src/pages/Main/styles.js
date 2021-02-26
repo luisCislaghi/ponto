87456a072ebb806218eec184232893b9760b7400
Luis Eduardo Cislaghi <luis.cislaghi@gmail.com>
2021-02-26 19:41:23 -0300

    fix: merge master into master

--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -47,31 +47,3 @@ export const ButtonContainer = styled.View`
   bottom: 40px;
   background-color: transparent;
 `;
-
-// export const Form = styled.View`
-//   flex-direction: row;
-//   margin-top: 10px;
-//   padding: 0 20px;
-// `;
-// export const Input = styled.TextInput.attrs({
-//   placeholderTextColor: '#999',
-// })`
-//   flex: 1;
-//   padding: 12px 15px;
-//   border-radius: 4px;
-//   font-size: 16px;
-//   background-color: #fdfdfd;
-// `;
-// export const Submit = styled.TouchableOpacity`
-//   background-color: #fd2;
-//   justify-content: center;
-//   margin-left: 10px;
-//   border-radius: 4px;
-//   padding: 0 14px;
-// `;
-// export const List = styled.FlatList.attrs({
-//   contentContainerStyle: {paddingHorizontal: 20},
-//   showVerticalScrollIndicator: false,
-// })`
-//   margin-top: 20px;
-// `;
